2ca7049488ff96d3fca758ea6f8461da4b23c2c4
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-11-26 19:23:14 +0200

    lots of fixes

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM node:latest
 
 MAINTAINER Arsen A.Gutsal <a.gutsal@softsky.com.ua>
 
-ADD index.js /opt/api/
 ADD .env /opt/api/
+ADD index.js /opt/api/
 ADD package.json /opt/api/
 RUN cd /opt/api && npm install
 
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
 const Promise = require("bluebird");
+const MongoClient = Promise.promisifyAll(require("mongodb").MongoClient);
+const ObjectId = require("mongodb").ObjectID;
+const rp = require('request-promise');
 const env = require('dotenv').config();
+const _ = require('lodash');
 
 const DATABASE_NAME = "hacked";
 
@@ -28,19 +30,46 @@ app.get("/breaches", (request, response) => {
     });
 });
 
-app.put("/user", (request, response) => {
+app.put("/user", async (request, response) => {
   console.log(request.body);
-  USER_COLLECTION.insertOne(request.body, (err, result) => {
-    if (err) throw err;
-    console.log('inserted');
-  })
+  const email = request.body.email;
+  if(email === undefined)
+    throw new Error('Email should be provided');
+
+  Promise.all([
+    rp.get({
+      uri:`https://haveibeenpwned.com/api/breachedaccount/${request.body.email}`,
+      headers: {
+        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+      },
+      json: true
+    }),
+    BADOO_COLLECTION
+      .findOneAsync({email:email})
+      .catch(err => response.status(404).send(err))
+      .then(result => new Promise(resolve => {
+        console.log('DATA:', result);
+        resolve(result);
+      }))
+  ])
+  // once all execeuted
+    .then(values => {
+      console.log('Values', values);
+      const data = _.merge(request.body, values.length === 2?values[1][0]:{});
+      values[0].foreach(it => {})
+
+      USER_COLLECTION.insertOneAsync(data)
+        .catch(console.error.bind(console))
+        .then(resp => response.send(resp))
+    });
 });
+
 app.post("/user", (request, response) => {
   console.log(request.body);
   const email = request.body.email;
   if(!email)
-    throw new Exception('Email should be provided');
-    USER_COLLECTION.updateOne({email: email}, {$set: request.body}, (err, result) => {
+    throw new Error('Email should be provided');
+  USER_COLLECTION.updateOne({email: email}, {$set: request.body}, (err, result) => {
     if (err) throw err;
     console.log('updated');
     return response.send(result);
@@ -48,13 +77,14 @@ app.post("/user", (request, response) => {
 });
 
 app.listen(3000, () => {
-    MongoClient.connect(process.env.CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-        if(error) {
-            throw error;
-        }
-        database = client.db(DATABASE_NAME);
-      console.log("Connected to `" + DATABASE_NAME + "`!");
-      BADOO_COLLECTION = Promise.promisifyAll(database.collection("badoo_ua"));
-      USER_COLLECTION = Promise.promisifyAll(database.collection("users"));
-    });
+  console.log('Connecting to:', process.env.CONNECTION_URL);
+  MongoClient.connect(process.env.CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+      throw error;
+    }
+    database = client.db(DATABASE_NAME);
+    console.log("Connected to `" + DATABASE_NAME + "`!");
+    BADOO_COLLECTION = Promise.promisifyAll(database.collection("badoo_ua"));
+    USER_COLLECTION = Promise.promisifyAll(database.collection("users"));
+  });
 });
