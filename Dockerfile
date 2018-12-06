3fd9a0580ab13dafbdff25301038ec40a708a1de
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-12-06 10:16:34 +0200

    cors,ssl support adeded, so

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,14 @@ FROM node:latest
 
 MAINTAINER Arsen A.Gutsal <a.gutsal@softsky.com.ua>
 
-ADD .env /opt/api/
-ADD index.js /opt/api/
-ADD package.json /opt/api/
-RUN cd /opt/api && npm install
+WORKDIR /opt/api
 
+ADD certs certs
+ADD .env .
+ADD index.js .
+ADD package.json .
+RUN npm install
+
+EXPOSE 8000
+EXPOSE 8443
 CMD ["node", "/opt/api/index.js"]
--- index.js
+++ index.js
@@ -1,33 +1,133 @@
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const Express = require("express");
 const BodyParser = require("body-parser");
 const Promise = require("bluebird");
-const MongoClient = Promise.promisifyAll(require("mongodb").MongoClient);
+const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
-const rp = require('request-promise');
+const rpc = require('request-promise-cache').use( require('bluebird').Promise )
 const env = require('dotenv').config();
 const _ = require('lodash');
 
 const DATABASE_NAME = "hacked";
 
 var BADOO_COLLECTION, USER_COLLECTION;
+const HTTP_PORT = env.parsed.HTTP_PORT
+const HTTPS_PORT = env.parsed.HTTPS_PORT;
+
+var options = {
+    key: fs.readFileSync( './certs/api.softsky.company.key' ),
+    cert: fs.readFileSync( './certs/api.softsky.company.cert' ),
+    requestCert: false,
+    rejectUnauthorized: false
+};
+
+const cors = require('cors')
 var app = Express();
+
+// after the code that uses bodyParser and other cool stuff
+var originsWhitelist = [
+  'http://localhost:4200',      //this is my front-end url for development
+  'http://api.softsky.company'
+];
+var corsOptions = {
+  origin: function(origin, callback){
+        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+        callback(null, isWhitelisted);
+  },
+  credentials:true
+}
+
+//here is the magic
+app.use(cors(corsOptions));
+
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
 var database;
+/**
+ * function, generates unique cacheKey fro request using `request.query` attributes
+ */
+const requestToCacheKey = (request) =>  Object.keys(request.query).sort().map(k => `${k}=${request.query[k]}`).join('&');
+
+app.get('/breachedaccount', (request, response) => {
+  const email = request.query.email;
+  const cacheKey = requestToCacheKey(request);
+  console.log(`Cache key: ${cacheKey}`);
+
+  Promise.all([
+    USER_COLLECTION
+      .findOneAsync({email:email})
+      .then(result =>
+            result?Promise.resolve(result.breaches):rpc(
+              {
+                url:`https://haveibeenpwned.com/api/v2/breachedaccount/${email}?truncateResponse=false`,
+                cacheKey: cacheKey,
+                cacheTTL: 3600,
+                headers: {
+                  "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+                },
+                json: true
+              })
+            .then((p) => {
+              USER_COLLECTION.insertOne({email: email, breaches:p});
+              return p;
+            })
+           )
+      .catch(err => err.statusCode === 404?[]:new Error(err)),
+    BADOO_COLLECTION
+      .findOneAsync({email:email})
+      .then(result => Promise.resolve(result))
+      .catch(err => response.status(404).send(err))
+  ])
+  // once all execeuted
+    .then(async (values) =>{
+      console.log('Values', values);
+
+      var Badoo = values[0].filter(it => it.Name === 'Badoo');
+      if((Badoo.length === 0) && values[1]){
+        Badoo = {
+          "Name": "Badoo",
+          "Title": "Badoo",
+          "Domain": "badoo.com",
+          "BreachDate": "2013-06-01",
+          "AddedDate": "2016-07-06T08:16:03Z",
+          "ModifiedDate": "2016-07-06T08:16:03Z",
+          "PwnCount": {
+            "$numberInt": "112005531"
+          },
+          "Description": "In June 2016, <a href=\"http://motherboard.vice.com/read/another-day-another-hack-user-accounts-of-dating-site-badoo\" target=\"_blank\" rel=\"noopener\">a data breach allegedly originating from the social website Badoo was found to be circulating amongst traders</a>. Likely obtained several years earlier, the data contained 112 million unique email addresses with personal data including names, birthdates and passwords stored as MD5 hashes. Whilst there are many indicators suggesting Badoo did indeed suffer a data breach, <a href=\"https://www.troyhunt.com/introducing-unverified-breaches-to-have-i-been-pwned\" target=\"_blank\" rel=\"noopener\">the legitimacy of the data could not be emphatically proven</a> so this breach has been categorised as &quot;unverified&quot;.",
+          "LogoType": "svg",
+          "DataClasses": [
+            "Dates of birth",
+            "Email addresses",
+            "Genders",
+            "Names",
+            "Passwords",
+            "Usernames"
+          ],
+          "IsVerified": false,
+          "IsFabricated": false,
+          "IsSensitive": true,
+          "IsRetired": false,
+          "IsSpamList": false,
+          "Data": values[1]
+        };
+        console.log(Badoo);
+        values[0].push(Badoo);
+      }
+      response.status(200).send(values[0]);
+    });
+});
 
 app.get("/breaches", (request, response) => {
+  const email = request.query.email;
   console.log(request.query);
-  BADOO_COLLECTION.findAsync(request.query)
-    .then((result) => {
-      result.toArray((err, res) => {
-        return response.send(res);
-      })
+  USER_COLLECTION.findOneAsync({email:email})
+    .then(result => {
     })
-    .catch(err => {
-      if (err) throw err;
-    });
 });
 
 app.put("/user", async (request, response) => {
@@ -36,32 +136,11 @@ app.put("/user", async (request, response) => {
   if(email === undefined)
     throw new Error('Email should be provided');
 
-  Promise.all([
-    rp.get({
-      uri:`https://haveibeenpwned.com/api/breachedaccount/${request.body.email}`,
-      headers: {
-        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-      },
-      json: true
-    }),
-    BADOO_COLLECTION
-      .findOneAsync({email:email})
-      .catch(err => response.status(404).send(err))
-      .then(result => new Promise(resolve => {
-        console.log('DATA:', result);
-        resolve(result);
-      }))
-  ])
-  // once all execeuted
-    .then(values => {
-      console.log('Values', values);
-      const data = _.merge(request.body, values.length === 2?values[1][0]:{});
-      values[0].foreach(it => {})
 
       USER_COLLECTION.insertOneAsync(data)
         .catch(console.error.bind(console))
         .then(resp => response.send(resp))
-    });
+    .catch(err => response.status(404).send(err));
 });
 
 app.post("/user", (request, response) => {
@@ -76,15 +155,17 @@ app.post("/user", (request, response) => {
   })
 });
 
-app.listen(3000, () => {
-  console.log('Connecting to:', process.env.CONNECTION_URL);
-  MongoClient.connect(process.env.CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-    if(error) {
-      throw error;
-    }
-    database = client.db(DATABASE_NAME);
-    console.log("Connected to `" + DATABASE_NAME + "`!");
-    BADOO_COLLECTION = Promise.promisifyAll(database.collection("badoo_ua"));
-    USER_COLLECTION = Promise.promisifyAll(database.collection("users"));
-  });
+MongoClient.connect(env.parsed.CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+  if(error) {
+    throw error;
+  }
+  database = client.db(DATABASE_NAME);
+  console.log("Connected to `" + env.parsed.CONNECTION_URL + "/" + DATABASE_NAME + "`!");
+  BADOO_COLLECTION = Promise.promisifyAll(database.collection("badoo"));
+  USER_COLLECTION = Promise.promisifyAll(database.collection("users"));
+  // ONCE Mongo connected we can run server
+
+  http.createServer(app).listen(HTTP_PORT, (it) => console.log(`Server on ${HTTP_PORT} started`));
+  https.createServer(app).listen(HTTPS_PORT, (it) => console.log(`Server on ${HTTPS_PORT} started`));
+
 });
